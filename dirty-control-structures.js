bc05f84504148f92c2cebd833801fa30415235ae
Kelly Murray <kellymurray@users.noreply.github.com>
2022-04-10 23:37:27 -0500

    Create dirty-control-structures.js

---
+++ dirty-control-structures.js
@@ -0,0 +1,35 @@
+function processTransactions(transactions) {
+  if (transactions && transactions.length > 0) {
+    for (const transaction of transactions) {
+      if (transaction.type === 'PAYMENT') {
+        if (transaction.status === 'OPEN') {
+          if (transaction.method === 'CREDIT_CARD') {
+            processCreditCardPayment(transaction);
+          } else if (transaction.method === 'PAYPAL') {
+            processPayPalPayment(transaction);
+          } else if (transaction.method === 'PLAN') {
+            processPlanPayment(transaction);
+          }
+        } else {
+          console.log('Invalid transaction type!');
+        }
+      } else if (transaction.type === 'REFUND') {
+        if (transaction.status === 'OPEN') {
+          if (transaction.method === 'CREDIT_CARD') {
+            processCreditCardRefund(transaction);
+          } else if (transaction.method === 'PAYPAL') {
+            processPayPalRefund(transaction);
+          } else if (transaction.method === 'PLAN') {
+            processPlanRefund(transaction);
+          }
+        } else {
+          console.log('Invalid transaction type!', transaction);
+        }
+      } else {
+        console.log('Invalid transaction type!', transaction);
+      }
+    }
+  } else {
+    console.log('No transactions provided!');
+  }
+}
