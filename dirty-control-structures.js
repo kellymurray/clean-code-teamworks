2253ba2db9361f8c4616c2fea1d2e6a92a1366cc
Kelly Murray <kellymurray@users.noreply.github.com>
2022-04-11 00:30:46 -0500

    update comments

--- dirty-control-structures.js
+++ dirty-control-structures.js
@@ -1,19 +1,19 @@
-function processTransactions(transactions) {
+function processTransactions(transactions) { // this processes transactions
   if (transactions && transactions.length > 0) {
     for (const transaction of transactions) {
       if (transaction.type === 'PAYMENT') {
         if (transaction.status === 'OPEN') {
           if (transaction.method === 'CREDIT_CARD') {
             processCreditCardPayment(transaction);
-          } else if (transaction.method === 'PAYPAL') {
+          } else if (transaction.method === 'PAYPAL') { // this is for cash transactions
             processPayPalPayment(transaction);
           } else if (transaction.method === 'PLAN') {
             processPlanPayment(transaction);
           }
         } else {
-          console.log('Invalid transaction type!');
+          console.log('Invalid transaction type!'); 
         }
-      } else if (transaction.type === 'REFUND') {
+      } else if (transaction.type === 'REFUND') { // does the thing for refunds
         if (transaction.status === 'OPEN') {
           if (transaction.method === 'CREDIT_CARD') {
             processCreditCardRefund(transaction);
